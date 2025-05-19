5cd259611463e1dab0070e8b9d41032ad86e39b3
naharran <nahar@proggio.com>
2025-05-19 18:02:42 +0300

    change route name of movie list in app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
      <BrowserRouter>
         <Routes>
-            <Route path="/movies" element={<MovieList/>} />
+            <Route path="/movies-list" element={<MovieList/>} />
             <Route path="add" element={<MovieForm/>}/>
             <Route path="edit/:id" element={<MovieForm/>}/>
             <Route path="/:movie" />
